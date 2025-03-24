1eecdff3958b788e39b70850d9f612bc50edb17a
Abhishak Kumar Malviya <57094659+akmalviya03@users.noreply.github.com>
2025-03-24 16:58:52 +0530

    Formatting (#1)
    * format files
    
    * updated documentation
    
    * format files

--- lib/better_dynamic_icon.dart
+++ lib/better_dynamic_icon.dart
@@ -1,2 +1,4 @@
-export 'package:better_dynamic_icon/src/implementation.dart' show BetterDynamicIcon,IconDetails;
-export 'package:better_dynamic_icon/src/implementation.dart' hide MethodChannelBetterDynamicIcon,BetterDynamicIconPlatform;
+export 'package:better_dynamic_icon/src/implementation.dart'
+    show BetterDynamicIcon, IconDetails;
+export 'package:better_dynamic_icon/src/implementation.dart'
+    hide MethodChannelBetterDynamicIcon, BetterDynamicIconPlatform;
--- lib/src/icon_details.dart
+++ lib/src/icon_details.dart
@@ -1,24 +1,29 @@
 part of 'implementation.dart';
 
-//Holds details of the icon
+///Holds details of the icon
 class IconDetails {
-  //Android -> Full Activity Name
-  //iOS -> AppIcon Name
+  ///Android -> Full Activity Name
+  ///iOS -> AppIcon Name
   final String accessName;
-  //Android -> android:label provided inside activity
-  //iOS -> imageasset name
+
+  ///Android -> android:label provided inside activity
+  ///iOS -> imageasset name
   final String label;
-  //Icon is currently active or not
+
+  ///Icon is currently active or not
   final bool enabled;
-  //Image data
+
+  ///Image data
   final Uint8List iconData;
 
+  ///Create IconDetails by specifying details
   IconDetails(
       {required this.accessName,
       required this.label,
       required this.enabled,
       required this.iconData});
 
+  ///Provide map of icon values
   factory IconDetails.fromJson(Map<dynamic, dynamic> values) {
     return IconDetails(
         accessName: values['name'] ?? "NA",
