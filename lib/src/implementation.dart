5fde2b83c6e70d9ad711028d13da97be46ca5830
Abhishak Kumar Malviya <abhishakkrmalviya@gmail.com>
2025-03-24 16:20:42 +0530

    updated documentation

--- lib/src/implementation.dart
+++ lib/src/implementation.dart
@@ -6,16 +6,16 @@ part 'icon_details.dart';
 part 'better_dynamic_icon_method_channel.dart';
 part 'better_dynamic_icon_platform_interface.dart';
 
-//Gives Access to the app icon change and fetch all icons
+///Gives Access to the app icon change and fetch all icons
 class BetterDynamicIcon {
-  //For android provide full path of the activity name
-  //For ios provide app icon name
+  ///For android provide full path of the activity name
+  ///For ios provide app icon name
   Future<String?> changeAppIcon(String iconName) {
     return BetterDynamicIconPlatform.instance.changeAppIcon(iconName);
   }
 
-  //Android will fetch all the icons associated with activity and activity-aliases.
-  //iOS will give us the list of imageassets along with the name.
+  ///Android will fetch all the icons associated with activity and activity-aliases.
+  ///iOS will give us the list of imageassets along with the name.
   Future<List<IconDetails>> getAllIcons() {
     return BetterDynamicIconPlatform.instance.getAllIcons();
   }
